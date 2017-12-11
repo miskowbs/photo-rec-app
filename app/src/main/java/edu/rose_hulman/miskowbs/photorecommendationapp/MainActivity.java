b322e219a2b7977110b700be3e6754e36c59bb03
Bart Miskowiec <miskowbs@rose-hulman.edu>
2017-12-10 20:41:54 -0500

    expanding MainActivity

--- app/src/main/java/edu/rose_hulman/miskowbs/photorecommendationapp/MainActivity.java
+++ app/src/main/java/edu/rose_hulman/miskowbs/photorecommendationapp/MainActivity.java
@@ -1,13 +1,94 @@
 package edu.rose_hulman.miskowbs.photorecommendationapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import edu.rose_hulman.miskowbs.photorecommendationapp.fragments.LoginFragment;
+
+public class MainActivity extends AppCompatActivity implements
+        LoginFragment.OnLoginListener, GoogleApiClient.OnConnectionFailedListener{
+
+    private static final int RC_SIGN_IN = 1;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private OnCompleteListener mOnCompleteListener;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        initializeListeners();
+        initializeGoogle();
+    }
+
+    private void initializeListeners() {
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                Log.d("TAG", "User: " + user);
+                if (user != null) {
+                    //SwitchToLandingFragment();
+                } else {
+                    //switchToLoginFragment();
+                }
+            }
+        };
+
+        mOnCompleteListener = new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if(!task.isSuccessful()) {
+                    showLoginError("Login Failed!");
+                }
+            }
+        };
+    }
+
+    private void initializeGoogle() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onGoogleLogin() {
+        //TODO: Implement this
+    }
+
+    private void showLoginError(String message) {
+        LoginFragment loginFragment = (LoginFragment) getSupportFragmentManager().findFragmentByTag("Login");
+        loginFragment.onLoginError(message);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        //TODO: Implement this
     }
 }
