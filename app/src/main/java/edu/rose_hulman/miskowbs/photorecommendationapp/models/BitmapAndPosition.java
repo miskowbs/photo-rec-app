f31aab6a235e661d5eeff60f4f391452a20fcb26
Bart Miskowiec <miskowbs@rose-hulman.edu>
2018-01-09 21:25:03 -0500

    adding new classes

--- app/src/main/java/edu/rose_hulman/miskowbs/photorecommendationapp/models/BitmapAndPosition.java
+++ app/src/main/java/edu/rose_hulman/miskowbs/photorecommendationapp/models/BitmapAndPosition.java
@@ -1,8 +1,26 @@
 package edu.rose_hulman.miskowbs.photorecommendationapp.models;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by miskowbs on 1/9/2018.
  */
 
 public class BitmapAndPosition {
+    private Bitmap bitmap;
+
+    private int position;
+
+    public BitmapAndPosition(Bitmap bitmap, int position) {
+        this.bitmap = bitmap;
+        this.position = position;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
--- app/src/main/java/edu/rose_hulman/miskowbs/photorecommendationapp/tasks/GetImageAndPositionTask.java
+++ app/src/main/java/edu/rose_hulman/miskowbs/photorecommendationapp/tasks/GetImageAndPositionTask.java
@@ -1,8 +1,51 @@
 package edu.rose_hulman.miskowbs.photorecommendationapp.tasks;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import edu.rose_hulman.miskowbs.photorecommendationapp.models.BitmapAndPosition;
+
 /**
  * Created by miskowbs on 1/9/2018.
  */
 
-public class GetImageAndPositionTask {
+public class GetImageAndPositionTask extends AsyncTask<String, Void, BitmapAndPosition> {
+    private ImageConsumer mImageConsumer;
+    private int mAdapterPosition;
+
+
+    public GetImageAndPositionTask(ImageConsumer activity, int adapterPosition) {
+        mImageConsumer = activity;
+        mAdapterPosition = adapterPosition;
+    }
+
+    @Override
+    protected BitmapAndPosition doInBackground(String... strings) {
+        String urlString = strings[0];
+        InputStream in;
+        Bitmap bitmap = null;
+        try {
+            in = new URL(urlString).openStream();
+            bitmap = BitmapFactory.decodeStream(in);
+        } catch (IOException e) {
+            Log.e("INPUT_STREAM", e.toString());
+        }
+        return new BitmapAndPosition(bitmap, mAdapterPosition);
+    }
+
+    @Override
+    protected void onPostExecute(BitmapAndPosition bitmap) {
+        super.onPostExecute(bitmap);
+        mImageConsumer.onImageLoaded(bitmap);
+    }
+
+    public interface ImageConsumer {
+        void onImageLoaded(BitmapAndPosition bitmap);
+    }
 }
